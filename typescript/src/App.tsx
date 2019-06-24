92cf927128cafebdee3dace06c579432d3321760
mrgromov <mailrxtx@gmail.com>
2019-06-25 00:00:05 +0400

    LimitedField stateful component created

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Message from './Message'
+import LimitedField from './LimitedField'
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <Message name="Sergio" message="Hi there folks!" />
+            <LimitedField maxLength={30} text="Enter >= 30 chars" />
         </div>
     )
 }
---
+++ typescript/src/LimitedField.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { JSXElement } from '@babel/types'
+
+const initialState = {
+    text: '',
+    charsCount: 0
+}
+
+type State = Readonly<typeof initialState>
+
+interface IProps {
+    maxLength?: number
+    text?: string
+}
+
+interface IOnChange {
+    (event: React.ChangeEvent<HTMLInputElement>): void
+}
+
+class LimitedField extends React.Component<IProps> {
+    constructor(props: IProps) {
+        super(props)
+        if (props.text) {
+            initialState.text = props.text
+            initialState.charsCount = props.text.length
+        }
+    }
+
+    readonly state: State = initialState
+
+    handleOnChange: IOnChange = (event) => {
+        event.preventDefault()
+        const { value } = event.target
+        const newState: State = { text: value, charsCount: value.length }
+        this.setState(newState)
+    }
+
+    verifyLength = (): string | JSX.Element | void => {
+        const length = this.state.text.length
+        const maxLength = this.props.maxLength || 50
+        if (length <= maxLength && maxLength - length <= 10) {
+            return `${maxLength - length} chars left`
+        }
+
+        if (length > maxLength) {
+            const message = `${length -
+                maxLength} chars over ${maxLength} in current field`
+            return <b>{message}</b>
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <input
+                    type="text"
+                    value={this.state.text}
+                    onChange={this.handleOnChange}
+                />
+                <span> ({this.state.charsCount} chars)</span>
+                <br />
+                {this.verifyLength()}
+            </React.Fragment>
+        )
+    }
+}
+
+export default LimitedField
